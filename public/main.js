532ab64379ee379b7e43ed3309cb7d3c32bf347e
rafajr91 <rah5024@gmail.com>
2017-10-30 16:12:51 -0700

    Made styling changes to document. Added background image. #1

--- public/main.js
+++ public/main.js
@@ -13,14 +13,13 @@ getMakes()
 
 const $input = document.querySelector('#typeahead')
 const $searchBox = document.querySelector('.search-box')
-const $searchItem = document.querySelector('.search-item')
 
 let matches = []
 
 const typeahead = () => {
   matches.splice(0, matches.length)
   for (var i = 0; i < carMakes.length; i++) {
-    if (carMakes[i].make.toLowerCase().startsWith($input.value.toLowerCase()) && $input.value !== null) {
+    if (carMakes[i].make.toLowerCase().startsWith($input.value.toLowerCase())) {
       matches.push(carMakes[i].make)
     }
   }
@@ -30,12 +29,13 @@ const renderSearchItems = (matches) => {
   return matches.map((match) => {
     const $searchItem = document.createElement('div')
     $searchItem.setAttribute('class', 'search-item')
+    $searchItem.setAttribute('data-name', match)
     $searchItem.textContent = match
     return $searchItem
   })
 }
 
-const appendItems = (matches) => {
+const appendMatches = (matches) => {
   return renderSearchItems(matches).reduce((parent, child) => {
     parent.appendChild(child)
     return parent
@@ -46,6 +46,13 @@ $input.addEventListener('input', (event) => {
   $searchBox.innerHTML = ''
   if (event.target.value !== '') {
     typeahead()
-    appendItems(matches)
+    appendMatches(matches)
+  }
+})
+
+$searchBox.addEventListener('click', (event) => {
+  if (event.target.className === 'search-item') {
+    $input.value = event.target.getAttribute('data-name')
+    $searchBox.innerHTML = ''
   }
 })
