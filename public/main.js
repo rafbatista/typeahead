8cb6e046440642420a05fcb2e9687a11ff9e9f58
rafajr91 <rah5024@gmail.com>
2017-10-30 11:51:24 -0700

    Made a function that filters out search input items into a new array. #1

--- public/main.js
+++ public/main.js
@@ -24,16 +24,3 @@ const typeahead = () => {
   }
   console.log(match)
 }
-
-typeahead()
-
-// match.splice(0, match.length)
-// for (var i = 0; i < carMakes.length; i++) {
-//   for (var j = 0; j < $input.value.length; j++) {
-//     if ($input.value[j].toLowerCase() === carMakes[i].make[j].toLowerCase() && $input.value[0].toLowerCase() === carMakes[i].make[0].toLowerCase()) {
-//       match.push(carMakes[i].make)
-//     }
-//   }
-// }
-// let newMatch = Array.from(new Set(match))
-// console.log(newMatch)
